1cedbdca4832ab0f01011657d64527b9ddc371b6
Henrique Tornelli Duarte <tornelli.henrique@gmail.com>
2022-08-05 14:40:38 -0300

    Add project files.

---
+++ GLExample/GLExample.cpp
@@ -0,0 +1,11 @@
+﻿// GLExample.cpp : Defines the entry point for the application.
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello GLExample." << std::endl;
+	std::cin.get();
+
+	return 0;
+}
